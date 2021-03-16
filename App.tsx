69092000c4514d0a7ce1eba0a86a39f04e39be3a
ChihYang41 <Jigsaw0401@gmail.com>
2021-03-16 13:28:38 +0800

    styling exercise done

--- App.tsx
+++ App.tsx
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Here are some boxes of different colors
+        </Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.text}>Cyan: #2aa198</Text>
+        </View>
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.text}>Blue: #268bd2</Text>
+        </View>
+        <View style={[styles.box, styles.magenta]}>
+          <Text style={styles.text}>Magenta: #d33682</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.text}>Orange: #cb4b16</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
+  box: {
+    borderRadius: 3,
+    padding: 10,
+    marginBottom: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  title: {
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  container: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+    flexDirection: 'row',
   },
 });
+
+export default App;
